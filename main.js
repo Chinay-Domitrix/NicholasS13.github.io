7c9a5ea2cd7bb1390076f780c415b734896f4cc6
NicholasS13 <nksahu@verizon.net>
2021-05-30 21:31:43 -0400

    added base for 3D website to new branch

---
+++ main.js
@@ -0,0 +1,22 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#canvas"),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+renderer.render(scene, camera);
+/*if ( WEBGL.isWebGLAvailable() ) {
+
+	// Initiate function or other initializations here
+	animate();
+
+} else {
+
+	const warning = WEBGL.getWebGLErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
+
+}TBD to check compatability*/ 
